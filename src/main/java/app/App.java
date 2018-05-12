8dd3461b130eb6d8e41fcf81fc48d329738e505a
Misha <antoshchuk.misha@gmail.com>
2018-05-12 12:45:58 -0400

    repo cleanup

---
+++ src/main/java/app/App.java
@@ -0,0 +1,13 @@
+package app;
+
+import utils.ConfigurationReader;
+
+public class App {
+    public static void main(String[] args) {
+        String url = ConfigurationReader.getProperty("url");
+        String sheetPath = ConfigurationReader.getProperty("sheet-path");
+        int batchNumber = Integer.parseInt(ConfigurationReader.getProperty("batch-number"));
+        BatchLoader loader = new BatchLoader(batchNumber, url);
+        loader.load(sheetPath);
+    }
+}
